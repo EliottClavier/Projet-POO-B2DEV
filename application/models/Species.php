5d4e716ce5cae524a1269c1f3b6860e545cfd7d8
Florent <florent.chabin@students.campus.academy>
2021-06-17 10:48:52 +0200

    correction arrays

--- application/models/Species.php
+++ application/models/Species.php
@@ -14,7 +14,7 @@ class Species extends APIData
     public $string_colors;
     public $language;
     public $homeworld;
-    public array  $people;
-    public array $films;
+    public $people = [];
+    public $films = [];
 
 }
